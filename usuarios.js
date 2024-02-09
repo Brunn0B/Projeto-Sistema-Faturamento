2c2914921348fafd57bbd2e665cce654e3ffa31b
Brunn0B <brito.brunnob@gmail.com>
2024-02-09 20:56:50 -0300

    Inplementação Base de testes FTU croesus

---
+++ usuarios.js
@@ -0,0 +1,12 @@
+var usuarios = {
+    'usuarioComum1': { email: 'usuario1@croesus.com.br', senha: '123', nome: 'teste' },
+    'usuarioComum2': { email: 'usuario2@croesus.com.br', senha: 'senhaComum2', nome: 'Usuário Comum 2' },
+    'usuarioComum3': { email: 'usuario3@croesus.com.br', senha: 'senhaComum3', nome: 'Usuário Comum 3' }
+};
+
+var admins = {
+    'adminUsuario1': { email: 'admin1@croesus.com.br', senha: '123', nome: 'Brunno' },
+    'adminUsuario2': { email: 'admin2@croesus.com.br', senha: 'senhaAdmin2', nome: 'Administrador 2' }
+};
+
+// Usuarios e Adms estao funciionando nao mecher!!
