200d5eb3a07cf583347389d7f8ac7602e34be675
Brunn0B <brito.brunnob@gmail.com>
2024-02-09 22:33:13 -0300

    Atualizaçao e correçao do loguin

--- usuarios.js
+++ usuarios.js
@@ -1,12 +1,10 @@
 var usuarios = {
-    'usuarioComum1': { email: 'usuario1@croesus.com.br', senha: '123', nome: 'teste' },
-    'usuarioComum2': { email: 'usuario2@croesus.com.br', senha: 'senhaComum2', nome: 'Usuário Comum 2' },
-    'usuarioComum3': { email: 'usuario3@croesus.com.br', senha: 'senhaComum3', nome: 'Usuário Comum 3' }
+    'usuarioComum1': { email: 'usuario1@croesus.com.br', senha: '123'},
+    'usuarioComum2': { email: 'usuario2@croesus.com.br', senha: 'senhaComum2',},
+    'usuarioComum3': { email: 'usuario3@croesus.com.br', senha: 'senhaComum3',}
 };
 
 var admins = {
-    'adminUsuario1': { email: 'admin1@croesus.com.br', senha: '123', nome: 'Brunno' },
-    'adminUsuario2': { email: 'admin2@croesus.com.br', senha: 'senhaAdmin2', nome: 'Administrador 2' }
+    'adminUsuario1': { email: 'admin1@croesus.com.br', senha: '123',},
+    'adminUsuario2': { email: 'admin2@croesus.com.br', senha: 'senhaAdmin2',}
 };
-
-// Usuarios e Adms estao funciionando nao mecher!!
